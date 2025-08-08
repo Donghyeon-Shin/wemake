3dc9195bf812636303ec56ce473f0feae4a1a63d
Donghyeon Shin <shindong0321@gmail.com>
2025-08-09 01:14:37 +0900

    feat : Navigation Bar 로그인, 로그아웃 UI 구현

--- app/common/components/layout/navigation.tsx
+++ app/common/components/layout/navigation.tsx
@@ -1,3 +1,11 @@
+import {
+  BarChart3Icon,
+  BellIcon,
+  LogOutIcon,
+  MessageCircleIcon,
+  SettingsIcon,
+  UserIcon,
+} from 'lucide-react';
 import { Link } from 'react-router';
 import {
   NavigationMenu,
@@ -10,6 +18,17 @@ import {
 } from '~/common/components/ui/navigation-menu';
 import { Separator } from '~/common/components/ui/separator';
 import { cn } from '../../../lib/utils';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { Button } from '../ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
 
 const menus: {
   name: string;
@@ -130,9 +149,17 @@ const menus: {
   },
 ];
 
-export default function Navigation() {
+export default function Navigation({
+  isLoggedIn,
+  hasNotifications,
+  hasMessages,
+}: {
+  isLoggedIn: boolean;
+  hasNotifications: boolean;
+  hasMessages: boolean;
+}) {
   return (
-    <nav className='flex px-20 h-16 items-center justify-between backdrop-blur-2xl fixed top-0 left-0 right-0 z-50 bg-background/50'>
+    <nav className='flex px-20 h-16 items-center justify-between backdrop-blur fixed top-0 left-0 right-0 z-50 bg-background/50'>
       <div className='flex items-center gap-4'>
         <Link to='/' className='font-bold tracking-tighter text-lg'>
           wemake
@@ -157,12 +184,6 @@ export default function Navigation() {
                               item.to === '/products/promote' &&
                                 'col-span-2 bg-primary/10 hover:bg-primary/20',
                               item.to === '/jobs/submit' &&
-                                'col-span-2 bg-primary/10 hover:bg-primary/20',
-                              item.to === '/community/create' &&
-                                'col-span-2 bg-primary/10 hover:bg-primary/20',
-                              item.to === '/teams/create' &&
-                                'col-span-2 bg-primary/10 hover:bg-primary/20',
-                              item.to === '/ideas' &&
                                 'col-span-2 bg-primary/10 hover:bg-primary/20',
                             )}
                           >
@@ -194,6 +215,77 @@ export default function Navigation() {
           </NavigationMenuList>
         </NavigationMenu>
       </div>
+      {isLoggedIn ? (
+        <div className='flex items-center gap-2'>
+          <Button size='icon' variant='ghost' asChild className='relative'>
+            <Link to='/my/notifications'>
+              <BellIcon className='size-4' />
+              {hasNotifications && (
+                <span className='absolute top-1.5 right-1.5 size-2 bg-red-500 rounded-full' />
+              )}
+            </Link>
+          </Button>
+          <Button size='icon' variant='ghost' asChild className='relative'>
+            <Link to='/my/messages'>
+              <MessageCircleIcon className='size-4' />
+              {hasMessages && (
+                <span className='absolute top-1.5 right-1.5 size-2 bg-red-500 rounded-full' />
+              )}
+            </Link>
+          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar>
+                <AvatarImage src='https://github.com/Donghyeon-shin.png' />
+                <AvatarFallback>N</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-56'>
+              <DropdownMenuLabel className='flex flex-col'>
+                <span className='font-semibold'>Donghyeon Shin</span>
+                <span className='text-xs text-muted-foreground'>shindong0321@gmail.com</span>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem asChild className='cursor-pointer'>
+                  <Link to='/my/dashboard'>
+                    <BarChart3Icon className='size-4 mr-2' />
+                    Dashboard
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild className='cursor-pointer'>
+                  <Link to='/my/profile'>
+                    <UserIcon className='size-4 mr-2' />
+                    Profile
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild className='cursor-pointer'>
+                  <Link to='/my/settings'>
+                    <SettingsIcon className='size-4 mr-2' />
+                    Settings
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild className='cursor-pointer'>
+                <Link to='/logout'>
+                  <LogOutIcon className='size-4 mr-2' />
+                  Logout
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      ) : (
+        <div className='flex items-center gap-4'>
+          <Button asChild variant='secondary'>
+            <Link to='/login'>Login</Link>
+          </Button>
+          <Button asChild>
+            <Link to='/auth/join'>Join</Link>
+          </Button>
+        </div>
+      )}
     </nav>
   );
 }
