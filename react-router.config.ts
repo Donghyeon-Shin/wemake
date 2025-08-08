55b014699f9001b2209908715edccd0d15b90058
Donghyeon Shin <shindong0321@gmail.com>
2025-08-08 16:27:56 +0900

    feat : React Router 7.0.1 초기 설정

---
+++ react-router.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from "@react-router/dev/config";
+
+export default {
+  // Config options...
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true, // 서버 사이드 렌더링 기본값, SPA 모드로 설정하려면 `false`로 설정
+} satisfies Config;
