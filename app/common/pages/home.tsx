b08845a911ab3920c10dd8d2ac1af0d8288a2b9d
Donghyeon Shin <shindong0321@gmail.com>
2025-08-08 18:08:06 +0900

    feat : home page 생성

---
+++ app/common/pages/home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className='pt-16 p-4 container mx-auto'>
+      <section className='space-y-2'>
+        <h1 className='text-2xl font-semibold'>Home</h1>
+        <p className='text-muted-foreground'>첫 화면입니다.</p>
+      </section>
+    </main>
+  );
+}
