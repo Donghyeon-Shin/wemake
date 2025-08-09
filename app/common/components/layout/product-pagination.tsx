e2978316be840d3a325d3160a401fd69e376cd01
Donghyeon Shin <shindong0321@gmail.com>
2025-08-10 01:01:44 +0900

    feat : Product Pagination UI 구현

--- app/common/components/layout/product-pagination.tsx
+++ app/common/components/layout/product-pagination.tsx
@@ -1,7 +1,100 @@
+import { useSearchParams } from 'react-router';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '../ui/pagination';
+
 type ProductPaginationProps = {
   totalPages: number;
 };
 
 export default function ProductPagination({ totalPages }: ProductPaginationProps) {
-  return <div>ProductPagination</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get('page')) ?? 1;
+  const onClick = (page: number) => {
+    searchParams.set('page', page.toString());
+    setSearchParams(searchParams);
+  };
+  if (isNaN(page) || page < 1 || page > totalPages) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Pagination>
+        <PaginationContent>
+          {page > 1 ? (
+            <>
+              <PaginationItem>
+                <PaginationPrevious
+                  to={`?page=${page - 1}`}
+                  onClick={(event) => {
+                    event.preventDefault(); // prevent the default link behavior
+                    onClick(page - 1);
+                  }}
+                />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink
+                  to={`?page=${page - 1}`}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    onClick(page - 1);
+                  }}
+                >
+                  {page - 1}
+                </PaginationLink>
+              </PaginationItem>
+            </>
+          ) : null}
+          <PaginationItem>
+            <PaginationLink
+              to={`?page=${page}`}
+              isActive
+              onClick={(event) => {
+                event.preventDefault();
+                onClick(page);
+              }}
+            >
+              {page}
+            </PaginationLink>
+          </PaginationItem>
+          {page < totalPages ? (
+            <>
+              <PaginationItem>
+                <PaginationLink
+                  to={`?page=${page + 1}`}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    onClick(page + 1);
+                  }}
+                >
+                  {page + 1}
+                </PaginationLink>
+              </PaginationItem>
+              {page + 1 === totalPages ? null : (
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+              )}
+              <PaginationItem>
+                <PaginationNext
+                  to={`?page=${page + 1}`}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    onClick(page + 1);
+                  }}
+                />
+              </PaginationItem>
+            </>
+          ) : null}
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
 }
