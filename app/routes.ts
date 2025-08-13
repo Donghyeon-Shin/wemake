dd049ff05ea089353c07b2b99e8168a4ac4f3ecc
Donghyeon Shin <shindong0321@gmail.com>
2025-08-13 19:52:17 +0900

    feat : Dashboard Layout 및 Layout Page UI 구현

--- app/routes.ts
+++ app/routes.ts
@@ -61,10 +61,12 @@ export default [
     route('/create', 'features/teams/pages/create.tsx'),
   ]),
   ...prefix('/my', [
-    ...prefix('/dashboard', [
-      index('features/users/pages/dashboard.tsx'),
-      route('/:ideas', 'features/users/pages/dashboard-ideas.tsx'),
-      route('/products/:productId', 'features/users/pages/dashboard-product.tsx'),
+    layout('features/users/layouts/dashboard-layout.tsx', [
+      ...prefix('/dashboard', [
+        index('features/users/pages/dashboard.tsx'),
+        route('/:ideas', 'features/users/pages/dashboard-ideas.tsx'),
+        route('/products/:productId', 'features/users/pages/dashboard-product.tsx'),
+      ]),
     ]),
     route('/profile', 'features/users/pages/my-profile.tsx'),
     route('/settings', 'features/users/pages/settings.tsx'),
