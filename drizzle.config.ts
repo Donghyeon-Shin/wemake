cd40b164efc3dbb0c1d7c40bede404f8836c1f3e
Donghyeon Shin <shindong0321@gmail.com>
2025-08-17 20:45:23 +0900

    feat : User profile trigger

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   schema: './app/features/**/schema.ts', // 데이터베이스 스키마 정의 파일 경로
-  out: './app/migrations', // drizzle kit이 생성한 sql 파일을 저장할 경로
+  out: './app/sql/migrations', // drizzle kit이 생성한 sql 파일을 저장할 경로
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
