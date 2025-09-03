7c5b3739887f8aefe79fbdc2255755fd731d5535
Donghyeon Shin <shindong0321@gmail.com>
2025-09-03 18:01:51 +0900

    feat : OTP login 기능 구현

--- app/features/auth/pages/otp-complete.tsx
+++ app/features/auth/pages/otp-complete.tsx
@@ -1,13 +1,44 @@
-import { Form } from 'react-router';
+import { Loader2Icon } from 'lucide-react';
+import { Form, redirect, useNavigation, useSearchParams } from 'react-router';
+import z from 'zod';
 import { Button } from '~/common/components/ui/button';
 import { InputPair } from '~/common/components/ui/input-pair';
+import { makeSSRClient } from '~/supa-client';
 import type { Route } from './+types/otp-complete';
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: 'Verify OTP | wemake' }];
 };
 
-export default function OtpComplete() {
+const FormSchema = z.object({
+  email: z.email('Invalid email format'),
+  otp: z.string('Invalid OTP format').min(6).max(6),
+});
+
+export const action = async ({ request }: Route.ActionArgs) => {
+  const formData = await request.formData();
+  const { success, data: parsedData, error } = FormSchema.safeParse(Object.fromEntries(formData));
+  if (!success) {
+    return {
+      fieldErrors: z.flattenError(error).fieldErrors,
+      verifyError: null,
+    };
+  }
+
+  const { email, otp } = parsedData;
+  const { client, headers } = makeSSRClient(request);
+  const { error: verifyError } = await client.auth.verifyOtp({ email, token: otp, type: 'email' });
+  if (verifyError) {
+    return { fieldErrors: null, verifyError: verifyError.message };
+  }
+  return redirect('/', { headers });
+};
+
+export default function OtpComplete({ actionData }: Route.ComponentProps) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const email = searchParams.get('email');
+  const navigate = useNavigation();
+  const isSubmitted = navigate.state === 'submitting' || navigate.state === 'loading';
   return (
     <div className='relative flex flex-col items-center justify-center h-full'>
       <div className='flex flex-col items-center justify-center gap-10 w-full max-w-md'>
@@ -15,16 +46,20 @@ export default function OtpComplete() {
           <h1 className='text-2xl font-semibold'>Confirm OTP</h1>
           <p className='text-sm text-muted-foreground'>Enter the 4-digit code sent to your email</p>
         </div>
-        <Form className='w-full space-y-4'>
+        <Form className='w-full space-y-4' method='post'>
           <InputPair
             label='Email'
             description='Enter your email'
+            defaultValue={email || ''}
             name='email'
             type='email'
             id='email'
             required
             placeholder='i.e wemake@gmail.com'
           />
+          {actionData && 'fieldErrors' in actionData && (
+            <p className='text-sm text-red-500'>{actionData.fieldErrors?.email?.join(', ')}</p>
+          )}
           <InputPair
             label='OTP'
             description='Enter the 4-digit code sent to your email'
@@ -34,9 +69,15 @@ export default function OtpComplete() {
             required
             placeholder='i.e 1234'
           />
-          <Button type='submit' className='w-full'>
-            Login
+          {actionData && 'fieldErrors' in actionData && (
+            <p className='text-sm text-red-500'>{actionData.fieldErrors?.otp?.join(', ')}</p>
+          )}
+          <Button type='submit' className='w-full' disabled={isSubmitted}>
+            {isSubmitted ? <Loader2Icon className='animate-spin' /> : 'Verify OTP'}
           </Button>
+          {actionData && 'verifyError' in actionData && (
+            <p className='text-sm text-red-500'>{actionData.verifyError}</p>
+          )}
         </Form>
       </div>
     </div>
