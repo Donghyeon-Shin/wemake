fc3f7cf50a9a99b1163bcba3ec30be3de5c61ad7
Donghyeon Shin <shindong0321@gmail.com>
2025-12-24 18:00:13 +0900

    Deploy : vercel config Setting

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,9 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config';
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true, // 서버 사이드 렌더링 기본값, SPA 모드로 설정하려면 `false`로 설정
+  presets: [vercelPreset],
 } satisfies Config;
